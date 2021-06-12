b884e19b67cdcd75849d20966fcbce2ff2fba879
rahul4567 <rahul.edu.in@gmail.com>
2021-06-12 07:09:38 +0530

    Merge intervals insert

--- merge_intervals.js
+++ merge_intervals.js
@@ -1,3 +1,13 @@
+/*
+Given a list of intervals, merge all the overlapping intervals to produce a list that has only mutually exclusive intervals.
+
+Example 1:
+
+Intervals: [[1,4], [2,5], [7,9]]
+Output: [[1,5], [7,9]]
+Explanation: Since the first two intervals [1,4] and [2,5] overlap, we merged them into
+one [1,5].
+*/
 class Interval {
   constructor(start, end) {
     this.start = start;
@@ -55,4 +65,46 @@ result = "";
 for(i=0; i < merged_intervals.length; i++) {
   result += merged_intervals[i].get_interval() + " ";
 }
-console.log(`Merged intervals: ${result}`)
+console.log(`Merged intervals: ${result}`);
+
+/*
+Given a list of non-overlapping intervals sorted by their start time, insert a given interval at the correct position and merge all necessary intervals to produce a list that has only mutually exclusive intervals.
+
+Example 1:
+
+Input: Intervals=[[1,3], [5,7], [8,12]], New Interval=[4,6]
+Output: [[1,3], [4,7], [8,12]]
+Explanation: After insertion, since [4,6] overlaps with [5,7], we merged them into one [4,7].
+Example 2:
+
+Input: Intervals=[[1,3], [5,7], [8,12]], New Interval=[4,10]
+Output: [[1,3], [4,12]]
+Explanation: After insertion, since [4,10] overlaps with [5,7] & [8,12], we merged them into [4,12].
+Example 3:
+
+Input: Intervals=[[2,3],[5,7]], New Interval=[1,4]
+Output: [[1,4], [5,7]]
+Explanation: After insertion, since [1,4] overlaps with [2,3], we merged them into one [1,4].
+*/
+
+function insert(intervals, new_interval) {
+  let merged = [],
+      i = 0;
+  //skip all intervals
+  while (i<intervals.length && intervals[i].end < new_interval.start) {
+    merged.push(intervals[i]);
+    i++;
+  }
+
+  while (i<intervals.length && intervals[i].start <= new_interval.end) {
+    new_interval.start = Math.min(intervals[i].start, new_interval.start);
+    new_interval.end = Math.max(intervals[i].end, new_interval.start);
+  }
+
+  merged.push(new_interval);
+
+  while(i<intervals.length) {
+    merged.push(intervals[i]);
+  }
+  return merged;
+}
